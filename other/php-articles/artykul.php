9e306d54b1daab7c7884930ad5a6666d960eccf2
Krzysztof Kraszewski <krzysztof@kraszewski.me>
2016-05-23 18:25:09 +0200

    articles list working, separate page for each article with views count working also

--- other/php-articles/artykul.php
+++ other/php-articles/artykul.php
@@ -9,23 +9,20 @@
 
 require_once('mysqli-connect.php');
 
+$id = $_GET['id'];
 
-$query = "SELECT * FROM articles";
+$updateQuery = "UPDATE articles SET views = views + 1 WHERE id = " . $id;
+$updateResponse = @mysqli_query($dbc, $updateQuery);
+
+$query = "SELECT * FROM articles WHERE `id`=" . $id;
 $response = @mysqli_query($dbc, $query);
 
 if($response) {
   echo '<ul>';
   while($row = mysqli_fetch_array($response)) {
       echo '<li>
-              <p>
-                <a href="/artykul.php?id=' . $row['id'] . '">' . 
-                  '<b>'
-                    . $row['title'] . ' (' . $row['views'] . ' wyświetleń)
-                  </b></a>
-              </p>
-              <p>'
-                . $row['content'] . '
-              </p>
+              <p><b>' . $row['title'] . ' (' . $row['views'] . ' wyświetleń)</b></p>
+              <p>' . $row['content'] . '</p>
             </li>
           </ul>';
     }
