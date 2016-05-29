e6972b1b9abe8e04b88ebee21263061f5e369f75
Krzysztof Kraszewski <krzysztof@kraszewski.me>
2016-05-30 00:29:21 +0200

    converting km to miles and miles to km dynamically on text change working

---
+++ inf/android/unit-converter/kek3/app/src/main/java/krszwsk/kek3/MainActivity.java
@@ -0,0 +1,68 @@
+package krszwsk.kek3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Switch unit = (Switch) findViewById(R.id.unit);
+        final EditText input = (EditText) findViewById(R.id.input);
+        final TextView output = (TextView) findViewById(R.id.output);
+
+        input.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(input.getText().length() > 0) {
+                    double inputValue = Float.valueOf(input.getText().toString());
+                    if(unit.isChecked()) {
+                        output.setText(String.valueOf(inputValue * 0.6214));
+                    }
+                    else {
+                        output.setText(String.valueOf(inputValue * 1.6));
+                    }
+                }
+                else {
+                    output.setText(":T");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        unit.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(input.getText().length() > 0) {
+                    double inputValue = Float.valueOf(input.getText().toString());
+                    if(isChecked) {
+                        output.setText(String.valueOf(inputValue * 0.6214));
+                    }
+                    else {
+                        output.setText(String.valueOf(inputValue * 1.6));
+                    }
+                }
+                else {
+                    output.setText(":T");
+                }
+            }
+        });
+    }
+}
