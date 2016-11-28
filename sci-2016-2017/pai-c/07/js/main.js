6b5bf8de225059c0c394ad25fbcc3b178c08138c
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-11-28 14:42:21 +0100

    pai-c/07 9/20

--- sci-2016-2017/pai-c/07/js/main.js
+++ sci-2016-2017/pai-c/07/js/main.js
@@ -2,3 +2,10 @@ function pierwszyKlik () {
   alert('Wywołano funkcję pierwszyKlik()')
 }
 
+function przeladowanie () {
+  location.reload()
+}
+
+function kolorowanie () {
+  document.body.style.backgroundColor = 'green'
+}
