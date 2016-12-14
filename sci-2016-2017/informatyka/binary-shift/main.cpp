fdf35dfe78b76f38ae1c108b8e768aafc88bce58
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-14 10:17:05 +0100

    swapCharacters()

--- sci-2016-2017/informatyka/binary-shift/main.cpp
+++ sci-2016-2017/informatyka/binary-shift/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 
+std::string swapCharacters(std::string word) {
+    for (int i = 0; i < word.length(); i += 2) {
+        if (word[i+1]) {
+            char temp = word[i];
+            word[i] = word[i + 1];
+            word[i + 1] = temp;
+        }
+    }
+
+    return word;
+}
+
 std::string xorString(std::string payload, char secret) {
     for (int i = 0; i < payload.length(); i++) {
         payload[i] ^= secret;
@@ -8,10 +20,15 @@ std::string xorString(std::string payload, char secret) {
 }
 
 int main() {
+    /*
     std::string payload = "siema";
     char secret = 'a';
     std::string encrypted = xorString(payload, secret);
     std::cout << "Encrypted: " << encrypted << std::endl;
     std::cout << "Decrypted: " << xorString(encrypted, secret) << std::endl;
+     */
+
+    std::cout << swapCharacters("serwus") << std::endl;
+
     return 0;
 }
