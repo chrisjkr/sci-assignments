a491bf43c22c3766bf3da01fd3c5c8af6e4c7d88
Krzysztof Kraszewski <krszwsk@gmail.com>
2016-12-15 14:58:27 +0100

    abd-sql/10 done, but not totally working

--- sci-2016-2017/abd-sql/10/index.php
+++ sci-2016-2017/abd-sql/10/index.php
@@ -13,12 +13,46 @@
         echo "Created table dane";
         
         $STH = $DBH->prepare("INSERT INTO dane (pesel, imie, nazwisko)
-                              VALUES (8903
-                              0312345, 'Andrzej', 'Biały'),
+                              VALUES (89030312345, 'Andrzej', 'Biały'),
                                      (72102176543, 'Małgorzata', 'Wiśniewska'),
                                      (02321204567, 'Błażej', 'Prążkowski');");
         $STH->execute();
         echo "Inserted data into table dane";
+
+        $STH = $DBH->prepare("
+        CREATE TABLE danerozszerzone (
+            wzrost int(4),
+            waga int(4),
+            kolor_oczu varchar(50),
+            pesel bigint NOT NULL,my
+            PRIMARY KEY (pesel),
+            FOREIGN KEY (pesel) REFERENCES dane(pesel)
+        );");
+
+        $file = file_get_contents('danerozszerzone.txt');
+        $rows = explode(PHP_EOL, $file);
+        $query = 'INSERT INTO daterozszerzone (pesel, wzrost, waga, kolor_oczu) VALUES';
+        for ($i = 1; $i < sizeof($rows); $i++) {
+            $values = explode(';', $rows[$i]);
+            if ($i == sizeof($rows) - 1) {
+                $query += " (" . $values[0] . ", " . $values[1] . ", " . $values[2] . ", '" . $values[3] . "');";
+            } else {
+                $query = $query . " (" . $values[0] . ", " . $values[1] . ", " . $values[2] . ", '" . $values[3] . "'),";
+            }
+        }
+        $STH = $DBH->prepare($query);
+        $STH->execute();
+        echo "Imported data from file.";
+
+        $query = $DBH->prepare('SELECT imie, nazwisko, wzrost
+                                FROM dane
+                                JOIN danerozszerzone
+                                ON dane.pesel = danerozszerzone.pesel
+                                WHERE danerozszerzone.wzrost > 170');
+        $query->execute();
+        while($row = $query->fetch(PDO::FETCH_OBJ)) {
+            echo "Imie: " . $row->imie . " Nazwisko: " . $row->nazwisko . "Wzrost: " . $row->wzrost;
+        }
     } catch (PDOException $e) {
         echo $e;
     }
